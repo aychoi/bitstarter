38088f84b62b239f13bb5b54c301d483711f64fa
Albert Choi <albertyunhochoi@gmail.com>
2013-07-29 05:33:38 +0000

    trying without the tilde

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send(
-      fs.readFileSync('~/index.html').toString()
+      fs.readFileSync('/index.html').toString()
   );
 });
 
