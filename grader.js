9029f1fa429ca0ebbd7a4585bb4592e1b5ccc585
Albert Choi <albertyunhochoi@gmail.com>
2013-08-09 07:56:41 +0000

    trying restler for the first time

--- grader.js
+++ grader.js
@@ -2,6 +2,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -13,6 +14,17 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUrlExists = function(url){
+    rest.get(url).on('complete',function(result){
+	if (result instanceof Error){
+	    console.log(Error);
+	    process.exit(1);
+	}
+	return(result);
+    }
+    
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -43,6 +55,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to the thing')
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
